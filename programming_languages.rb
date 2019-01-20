8e6d17a7eaa95eac432b1a438a7797adf210ff6e
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-20 21:24:10 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,17 +1,25 @@
 require 'pry'
 def reformat_languages(languages)
+new_hash = {}
+  languages.each do |key, value| #keys = :oo or :functional
+    value.each do |progkey, progval| #progkey = :ruby, progval = {}
 
-languages.each_with_object({}) do | language_type, hash_new|
-  style = language_type.first #oo equates the hash with values whose style is oo
-  langs = language_type.last #oo equates the hash with values whose style is functional
+        progval.each do |type, tval|
 
-  langs.each do |spec_lang, lang_char|
-    if hash_new[spec_lang]
-      hash_new[spec_lang][:style] << style
-    else
-    hash_new[spec_lang] = { type: lang_char[:type], style: [style]}
-#you can make a string an array by calling it inside square brackets
-    end
-    end
-  end
-end
+            if new_hash.has_key?(progkey)
+              new_hash[progkey][:style] << [key]
+
+
+            else
+              new_hash[progkey] = {}
+              new_hash[progkey][type] = tval
+              new_hash[progkey][:style] = [key]
+              
+            end
+        end
+new_hash[progkey][:style] =  new_hash[progkey][:style].flatten
+    end #end do
+  end #end do
+
+new_hash
+end #end def
