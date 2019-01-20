938f4cac4f4f78b7727a870fbee030b37704fe18
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-20 21:23:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+
+languages.each_with_object({}) do | language_type, hash_new|
+  style = language_type.first #oo equates the hash with values whose style is oo
+  langs = language_type.last #oo equates the hash with values whose style is functional
+
+  langs.each do |spec_lang, lang_char|
+    if hash_new[spec_lang]
+      hash_new[spec_lang][:style] << style
+    else
+    hash_new[spec_lang] = { type: lang_char[:type], style: [style]}
+#you can make a string an array by calling it inside square brackets
+    end
+    end
+  end
 end
